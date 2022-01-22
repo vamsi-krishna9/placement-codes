114f24b9c2a7512a7a5bf462f835764bf85020a0
vamsi-krishna9 <95500203+vamsi-krishna9@users.noreply.github.com>
2022-01-22 20:43:51 +0530

    Create array_revrecrs.cpp

---
+++ array_revrecrs.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void rev_array(int arr[],int start,int end)
+{
+    int t;
+    if(start>=end)
+    {
+        return;
+    }
+    t = arr[start];
+    arr[start] = arr[end];
+    arr[end] = t;
+    start++;
+    end--;
+    rev_array(arr, start, end);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin >> *(arr+i);
+    }
+    rev_array(arr,0,n-1);
+    for(int i=0;i<n;i++)
+    {
+        cout << *(arr+i) << endl;
+    }
+}
